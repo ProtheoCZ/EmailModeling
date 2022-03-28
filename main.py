db14f33d92fe859264be7321cf2fe93f5ad6a835
Tomas Jirku <tomas.jirku@plus4u.net>
2022-03-28 15:06:53 +0200

    test commit 2

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(30+6)
+    xpoints = np.array([1, 8])
+    ypoints = np.array([3, 10])
+
+    plt.plot(xpoints, ypoints)
+    plt.show()
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
