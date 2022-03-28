f52878419be2828c6ac07cd258cf21e6bbab7470
Tomas Jirku <tomas.jirku@plus4u.net>
2022-03-28 13:41:05 +0200

    test commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(30+5)
+    print(30+6)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
