8b08f4b18c41cd1c49e2e25eb33d97d41684b3c5
Tomas Jirku <tomas.jirku@plus4u.net>
2022-06-27 10:33:48 +0200

    dimacs loader

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 import matplotlib.pyplot as plt
-import numpy as np
-
-if __name__ == '__main__':
-    xpoints = np.array([1, 8])
-    ypoints = np.array([3, 10])
-
-    plt.plot(xpoints, ypoints)
-    plt.show()
+import networkx as nx
 
 
+def load_site():
+    pass
 
+
+def add_node_params():
+    pass
+    # output/input trust, dict s neighbor nodes
+
+
+def load_dimacs(filename):
+    file = open(filename, 'r')
+    lines = file.readlines()
+
+    graph = nx.Graph()
+    counter = 0
+    for line in lines:
+        counter += 1
+        if counter >= 3:
+            temp_arr = line.split()
+            graph.add_edge(temp_arr[0], temp_arr[1])
+    return graph
+
+
+# G = nx.Graph()
+# G.add_nodes_from([1, 2, 3, 4, 5])
+# G.add_edges_from([(3, 2), (2, 3), (2, 4), (4, 5), (1, 4), (1, 2)])
+# print(G.number_of_edges(), G.number_of_nodes())
+# print(list(G.nodes))
+# print(list(G.edges))
+# nx.set_node_attributes(G, {2: "red", 1: "blue"}, name="color")
+# print(nx.get_node_attributes(G, 'color')[2])
+
+G = load_dimacs('C125-9.txt')
+nx.draw_networkx(G, None, True, True)
+plt.show()
--- sandbox.py
+++ sandbox.py
@@ -1,1 +1,13 @@
-# test
+import numpy as np
+
+if __name__ == '__main__':
+    print("testy")
+
+    x = np.array((1, 2, 3, 5, 8, 6, 7, 10))
+    print(x)
+
+    print(x.reshape((4, 2)))
+
+
+
+
