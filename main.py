48de8737a90f66210494dca43a86b0eb1f30a5ad
ProtheoCZ <tomas.jirku@atlas.cz>
2023-01-06 15:30:18 +0100

    barabasi albert generator prototype

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import json
+
 import matplotlib.pyplot as plt
 import networkx as nx
 import os
-
-
 
 def load_site():
     pass
@@ -26,6 +26,45 @@ def load_dimacs(filename):
             graph.add_edge(temp_arr[0], temp_arr[1])
     return graph
 
+def generate_barabasi_albert_graph(n, m, filepath):
+    graph = nx.barabasi_albert_graph(n, m)
+    json_graph = networkx_to_json(graph)
+    out_file = open(filepath, "w")
+    json.dump(json_graph, out_file)
+
+def networkx_to_json(graph):
+    #Todo rewrite
+    ret_json = {
+        "nodes": [],
+        "edges": []
+    }
+    for node in graph.nodes:
+        # print(node)
+        json_node = {
+            "label": graph.nodes[node]["label"],
+            "x": graph.nodes[node]["x"],
+            "y": graph.nodes[node]["y"],
+            "id": graph.nodes[node]["id"],
+            "attributes": {},
+            "color": graph.nodes[node]["displayed_color"],
+            "size": graph.nodes[node]["size"]
+        }
+        ret_json["nodes"].append(json_node)
+
+    for edge in graph.edges:
+        # print(edge[0])
+        # print(edge)
+        json_edge = {
+            "source": edge[0],
+            "target": edge[1],
+            "id": graph.edges[edge]["id"],
+            "attributes": {},
+            "color": graph.edges[edge]["displayed_color"],
+            "size": graph.edges[edge]["size"],
+        }
+        ret_json["edges"].append(json_edge)
+
+    return ret_json
 
 # G = nx.Graph()
 # G.add_nodes_from([1, 2, 3, 4, 5])
@@ -37,6 +76,6 @@ def load_dimacs(filename):
 # print(nx.get_node_attributes(G, 'color')[2])
 
 
-G = load_dimacs('C125-9.mtx')
-nx.draw_networkx(G, None, True, True)
-plt.show()
+# G = load_dimacs('C125-9.mtx')
+# nx.draw_networkx(G, None, True, True)
+# plt.show()
