2ca85fc7bd9a98867292264d7418c10fa2878786
Tomas Jirku <tomas.jirku@plus4u.net>
2023-06-23 09:45:04 +0200

    duplicate_check finished

--- duplicate check.py
+++ duplicate check.py
@@ -1,14 +1,20 @@
 
 
 nodes = []
+edges = []
 line_no = 0
-with open('soc-pokec/soc-pokec-relationships.txt') as file:
+with open('email-Eu-core.txt') as file:
     for line in file:
         line_no += 1
         temp_array = line.split()
         for node in temp_array:
             if node not in nodes:
                 nodes.append(node)
+
+        if [temp_array[0], temp_array[1]] not in edges and [temp_array[1], temp_array[0]] not in edges:
+            edges.append(temp_array)
         if line_no % 100000 == 0:
             print(str(line_no))
+
 print("nodes: " + str(len(nodes)) + " lines: " + str(line_no))
+print("edges: " + str(len(edges)))
